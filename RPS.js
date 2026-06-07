8a06b3869a170f01a3ccdb8284c73474be1b5007
soham-demo <sohamdesai206@gmail.com>
2026-06-07 15:25:32 +0530

    no comments

--- RPS.js
+++ RPS.js
@@ -14,15 +14,15 @@ const resetContainer = document.querySelector(".reset-container");
 
 const genComputerChoice = () => {
     // rock, paper, scissor
-    const options = ["rock","paper","scissor"];
-    const randIdx = Math.floor(Math.random()*3);
+    const options = ["rock", "paper", "scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
     /* generates random number from 0-1 but if we multiply it by n. i.e Math.random() * n , then we will get a range from 0-(n-1). Example if we multiply it by 3 then range will be 0-2 and imp to note that these no's. will be in floating values.
     If we want a integer no. then , 
     Math.floor(Math.random()) will give integer no. (i.e before decimal no. bcoz we use floor() ).
     Math.floor(Math.random() * 10) range will be 0-9 with interger number. 
     */
     return options[randIdx];
- };
+};
 
 const drawGame = () => {
     msg.innerText = "Game Draw";
@@ -30,14 +30,14 @@ const drawGame = () => {
     msg.classList.add("msg-draw");
 };
 
-const showWinner = (userWin,userChoice,compChoice) =>{
+const showWinner = (userWin, userChoice, compChoice) => {
     msg.className = ""; // clear old outcome/animation classes
-    if(userWin){
+    if (userWin) {
         userScore++;
-        userScorePara.innerText=userScore;
+        userScorePara.innerText = userScore;
         msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
         msg.classList.add("msg-win");
-        
+
         // Confetti Celebration
         if (typeof confetti === "function") {
             confetti({
@@ -46,12 +46,12 @@ const showWinner = (userWin,userChoice,compChoice) =>{
                 origin: { y: 0.7 }
             });
         }
-    }else{
+    } else {
         compScore++;
-        compScorePara.innerText=compScore;
+        compScorePara.innerText = compScore;
         msg.innerText = `You Lose. ${compChoice} beats your ${userChoice}`;
         msg.classList.add("msg-lose");
-        
+
         // Tactile Shake Feedback
         msg.classList.add("shake-anim");
         setTimeout(() => {
@@ -66,25 +66,25 @@ const showWinner = (userWin,userChoice,compChoice) =>{
 const playGame = (userChoice) => {
     const compChoice = genComputerChoice();
 
-    if(userChoice === compChoice){
+    if (userChoice === compChoice) {
         drawGame(); // Draw Game 
-    } else{
+    } else {
         let userWin = true;
-        if(userChoice ==="rock"){
-            userWin = compChoice==="paper"? false : true;
-        }else if(userChoice === "paper"){
-        userWin = compChoice==="scissor" ? false : true;
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissor" ? false : true;
         } else {
-            userWin = compChoice==="rock" ? false : true;
+            userWin = compChoice === "rock" ? false : true;
         }
-        showWinner(userWin,userChoice,compChoice);
+        showWinner(userWin, userChoice, compChoice);
     }
 };
 
 choices.forEach((choice) => {
-    choice.addEventListener("click",() => {
-    const userChoice = choice.getAttribute("id");
-      playGame(userChoice); 
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
     });
 });
 
